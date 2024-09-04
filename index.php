96a1455929ec7840e793be3e1f4cb41b3cae2b0c
unknown <badeghaju@gmail.com>
2024-09-04 10:32:01 +0545

    first commit

---
+++ index.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<!-- Created By CodingNepal -->
+<html lang="en" dir="ltr">
+   <head>
+      <meta charset="utf-8">
+      <title>Food Ordering System</title>
+      <link rel="stylesheet" href="style.css">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+   </head>
+   <body>
+      <!-- Start navbar -->
+      <nav>
+         <div class="menu-icon">
+            <span class="fas fa-bars"></span>
+         </div>
+         <div class="logo">
+            MithoFood
+         </div>
+         <div class="nav-items">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Menu</a></li>
+            <li><a href="#">Gallery</a></li>
+            <li><a href="#">About Us</a></li>
+            <li><a href="#">Contact Us</a></li>
+            <li><a href="login.php">Login</a></li>
+         </div>
+         <div class="cancel-icon">
+            <span class="fas fa-times"></span>
+         </div>
+      </nav>
+      <!-- End navbar -->
+
+      <!-- Start header -->
+        <header>
+            <div class="header__content">
+              <h2>"Hard Work should be rewarded by Good Food."</h2>
+              <h1>Mitho Food</h1>
+            </div>
+            <div class="header__image">
+              <img src="assets/momo.png" alt="header" />
+            </div>
+          </header>
+      <!-- End header -->  
+
+      <!-- Starts menu -->
+    <div class="menu">
+      <div class="heading">
+          <h1>POPULAR</h1>
+          <h3>&mdash; MENU &mdash; </h3>
+      </div>
+      <div class="food-items">
+          <img src="assets/aloo_chop.jpg">
+          <div class="details">
+              <div class="details-sub">
+                  <h5>Aloo Chop</h5>
+                  <h5 class="price"> 200 </h5>
+              </div>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit reiciendis nam non quia! Earum eveniet minus. Facilis explicabo natus nihil voluptatem eveniet pariatur.</p>
+              <button>Add To Cart</button>
+          </div>
+      </div>
+
+      <div class="food-items">
+          <img src="assets/chicken_chili.jpg">
+          <div class="details">
+              <div class="details-sub">
+                  <h5>Chicken Chili</h5>
+                  <h5 class="price"> 300 </h5>
+              </div>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit dolor sit amet consectetur adipisicing elit.</p>
+              <button>Add To Cart</button>
+          </div>
+      </div>
+
+      <div class="food-items">
+          <img src="assets/chicken_pakora.jpg">
+          <div class="details">
+              <div class="details-sub">
+                  <h5>Chicken Pakora</h5>
+                  <h5 class="price"> 400 </h5>
+              </div>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus quibusdam facilis, magni consectetur necessitatibus.</p>
+              <button>Add To Cart</button>
+          </div>
+      </div>
+
+      <div class="food-items">
+          <img src="assets/duck_choila.jpg">
+          <div class="details">
+              <div class="details-sub">
+                  <h5>Duck Choila</h5>
+                  <h5 class="price"> 500 </h5>
+              </div>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit lorem ipsum, dolor sit amet consectetur adipisicing elit</p>
+              <button>Add To Cart</button>
+          </div>
+      </div>
+
+      <div class="food-items">
+          <img src="assets/rice_pudding.jpg">
+          <div class="details">
+              <div class="details-sub">
+                  <h5>Rice Pudding</h5>
+                  <h5 class="price"> 200 </h5>
+              </div>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit</p>
+              <button>Add To Cart</button>
+          </div>
+      </div>
+
+      <div class="food-items">
+          <img src="assets/nepali_doughnuts.jpg">
+          <div class="details">
+              <div class="details-sub">
+                  <h5>Nepali Doughnuts</h5>
+                  <h5 class="price"> 100 </h5>
+              </div>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus reiciendis nam non quia! Earum eveniet quia minus nemo.</p>
+              <button>Add To Cart</button>
+          </div>
+      </div>
+  </div>
+  <!-- Ends menu -->
+
+      <!-- <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus vitae et atque? Ipsum, voluptatibus ducimus ex iure voluptas blanditiis eum facere, quisquam id, harum fugiat. Pariatur, earum. Quas, laboriosam nam.</p> -->
+       <script src="main.js"></script> 
+</body>
+</html>
+
